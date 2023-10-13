e9da9bce64027c1e06a67b07c0071421cf19bd6f
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-10-13 23:34:26 +0300

    Create Healthcare-Provider-Model.dart

---
+++ Health_care_app/Data-Models/Healthcare-Provider-Model.dart
@@ -0,0 +1,13 @@
+class HealthcareProvider {
+  final String name;
+  final String specialty;
+  final String address;
+  final String phoneNumber;
+
+  HealthcareProvider({
+    required this.name,
+    required this.specialty,
+    required this.address,
+    required this.phoneNumber,
+  });
+}
