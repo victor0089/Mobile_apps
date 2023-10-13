e66eea0912c182a499028223449c36626f418c99
Victor Adly <42696918+victor0089@users.noreply.github.com>
2023-10-13 23:32:41 +0300

    Create User-Information-Model.dart

---
+++ Health_care_app/Data-Models/User-Information-Model.dart
@@ -0,0 +1,15 @@
+class User {
+  final String uid;  // Unique user ID
+  final String email;
+  final String name;
+  final int age;
+  final String gender;
+
+  User({
+    required this.uid,
+    required this.email,
+    required this.name,
+    required this.age,
+    required this.gender,
+  });
+}
